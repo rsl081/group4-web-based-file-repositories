2c2bd61cd4132bbb9a7c44b6c142ad1a75e218ce
rsl081 <53944109+rsl081@users.noreply.github.com>
2022-03-27 13:55:32 +0800

    initial commit

---
+++ apps/faculty/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'group4-web-based-file-repositories-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'faculty';
+}
