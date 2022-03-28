0db242c949a01ab8d4b7c6e137586aa35f5eb0be
Jayson San Buenaventura <94850885+jcsnbnvntr22@users.noreply.github.com>
2022-03-29 01:53:41 +0800

    Initialize admin dashboard component

---
+++ apps/admin/src/app/admin-dashboard-card/admin-dashboard-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-dashboard-card',
+  templateUrl: './admin-dashboard-card.component.html',
+  styleUrls: ['./admin-dashboard-card.component.css']
+})
+export class AdminDashboardCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminDashboardCardComponent } from './admin-dashboard-card/admin-dashboard-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AdminDashboardComponent, AdminDashboardCardComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
