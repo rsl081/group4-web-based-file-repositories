2a039c13283a941af74d80cbbbfe02e723d0d57a
rsl081 <53944109+rsl081@users.noreply.github.com>
2022-03-28 11:39:31 +0800

    first init

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'faculty-root',
+  selector: 'admin-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'faculty';
+  title = 'admin';
 }
